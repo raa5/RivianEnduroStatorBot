0a4cd1320a90c36eadb08e96f269067bed278a41
Rohen Agarwal <rohenagarwal@rivian.com>
2025-03-09 13:59:05 -0500

    trial

--- RivianEnduroStatorBot.py
+++ RivianEnduroStatorBot.py
@@ -83,7 +83,7 @@ def job():
 
     one_hour_before = datetime.now() - timedelta(hours=1)
     recorded_at = one_hour_before.strftime("%Y-%m-%d %H:00")
-    eight_hours_before = datetime.now() - timedelta(hours=200)
+    eight_hours_before = datetime.now() - timedelta(hours=8)
     recorded_at_summary = eight_hours_before.strftime("%Y-%m-%d %H:00")
 
     # Define the queries
