92a9b1234363958b5ac2ca2212c964629b3f0271
Rohen Agarwal <rohenagarwal@rivian.com>
2025-03-08 19:33:03 -0600

    Formatted

--- RivianEnduroStatorBot.py
+++ RivianEnduroStatorBot.py
@@ -1,3 +1,6 @@
+########################################################################################
+# Import libraries
+########################################################################################
 import pandas as pd
 import os
 import requests
@@ -11,38 +14,51 @@ from slack_sdk.errors import SlackApiError
 from pyspark.sql import SparkSession
 from databricks import sql
 
+########################################################################################
 # Databricks Configuration
+########################################################################################
 DATABRICKS_SERVER_HOSTNAME = "rivian-prod-us-west-2.cloud.databricks.com"
 DATABRICKS_HTTP_PATH = "/sql/1.0/warehouses/b050a7573faba9ab"
-# DATABRICKS_ACCESS_TOKEN = "dapi0eb9a92a05ca6d1eb0a444b125490361"
 DATABRICKS_ACCESS_TOKEN = os.getenv("DATABRICKS_ACCESS_TOKEN")
+# DATABRICKS_ACCESS_TOKEN = "dapi0eb9a92a05ca6d1eb0a444b125490361"
 
-# slack_token = "xoxb-2995242172-7567570817185-SBLzNdVOIHNolyQszfZHMBbu"
 slack_token = os.getenv("SLACK_TOKEN")
+url = os.getenv("URL")
+# slack_token = "xoxb-2995242172-7567570817185-SBLzNdVOIHNolyQszfZHMBbu"
 # url = 'https://hooks.slack.com/services/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'
 # url = 'https://hooks.slack.com/services/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'
-url = os.getenv("URL")
 
+########################################################################################
 # Slack setup
+########################################################################################
 client = WebClient(token=slack_token)
 
+
+########################################################################################
+# Function To Send Message TO Slack
+########################################################################################
 def send_message_to_slack(channel, text):
     try:
-        response = client.chat_postMessage(
-            channel=channel,
-            text=text
-        )
+        response = client.chat_postMessage(channel=channel, text=text)
         print(f"Message sent to {channel} with timestamp {response['ts']}")
     except SlackApiError as e:
         print(f"Error sending message to Slack: {e.response['error']}")
 
+
+########################################################################################
+# Function to Connect to Databricks
+########################################################################################
 def create_databricks_connection():
     return sql.connect(
         server_hostname=DATABRICKS_SERVER_HOSTNAME,
         http_path=DATABRICKS_HTTP_PATH,
-        access_token=DATABRICKS_ACCESS_TOKEN
+        access_token=DATABRICKS_ACCESS_TOKEN,
     )
 
+
+########################################################################################
+# Function to Execute Query and Get Results
+########################################################################################
 def execute_query(query, conn):
     with conn.cursor() as cursor:
         cursor.execute(query)
@@ -50,6 +66,10 @@ def execute_query(query, conn):
         columns = [desc[0].upper() for desc in cursor.description]
         return pd.DataFrame(result, columns=columns)
 
+
+########################################################################################
+# Function defining all queries to run every hour
+########################################################################################
 def job():
     t0 = time.time()
     conn = create_databricks_connection()
@@ -59,8 +79,11 @@ def job():
     local_now = utc_now.astimezone(local_tz)  # Convert to local timezone
 
     one_hour_before = datetime.now() - timedelta(hours=1)
-    recorded_at = one_hour_before.strftime('%Y-%m-%d %H:00')
+    recorded_at = one_hour_before.strftime("%Y-%m-%d %H:00")
 
+    ########################################################################################
+    # Query 20 - Every Hour
+    ########################################################################################
     # Define the queries
     query_20 = f"""
     select 
@@ -139,7 +162,9 @@ def job():
     and alarm_description ilike ('%Axis not in control [ PaperPusher (Z6)%') 
     group by STATION_NAME
     """
-
+    ########################################################################################
+    # Query 40 - Every Hour
+    ########################################################################################
     query_40 = f"""
     SELECT COUNT(distinct product_serial) as COUNT, STATION_NAME, PARAMETER_NAME
     FROM manufacturing.spinal.fct_spinal_parameter_records
@@ -152,6 +177,9 @@ def job():
     GROUP BY STATION_NAME, PARAMETER_NAME
     """
 
+    ########################################################################################
+    # Query 50 - Every Hour
+    ########################################################################################
     query_50 = f"""
     SELECT 
         COUNT(*) as COUNT,
@@ -169,7 +197,10 @@ def job():
     WHERE activated_at > prev_cleared_at + INTERVAL '30 seconds'
        OR prev_cleared_at IS NULL; -- Keep the first occurrence
     """
-    
+
+    ########################################################################################
+    # Query 60 - Every Hour
+    ########################################################################################
     query_60 = f"""
     SELECT 
           COUNT(*) as COUNT,
@@ -182,7 +213,10 @@ def job():
     AND alarm_description ILIKE '%Assembly error :%'
     group by STATION_NAME
     """
-    
+
+    ########################################################################################
+    # Query 65 - Every Hour
+    ########################################################################################
     query_65 = f"""
     SELECT COUNT(DISTINCT product_serial) as COUNT, STATION_NAME, PARAMETER_NAME
     FROM manufacturing.spinal.fct_spinal_parameter_records
@@ -205,6 +239,9 @@ def job():
     ORDER BY COUNT DESC
     """
 
+    ########################################################################################
+    # Query 80 - Every Hour
+    ########################################################################################
     query_80 = f"""
     SELECT 
           COUNT(*) as COUNT,
@@ -231,6 +268,9 @@ def job():
     group by STATION_NAME
     """
 
+    ########################################################################################
+    # Query 100 - Every Hour
+    ########################################################################################
     query_100 = f"""
     SELECT COUNT(DISTINCT product_serial) as COUNT, STATION_NAME, PARAMETER_NAME
     FROM manufacturing.spinal.fct_spinal_parameter_records
@@ -256,6 +296,9 @@ def job():
     ORDER BY COUNT DESC
     """
 
+    ########################################################################################
+    # Query 110 - Every Hour
+    ########################################################################################
     query_110 = f"""
     SELECT COUNT(DISTINCT product_serial) as COUNT, STATION_NAME, PARAMETER_NAME
     FROM manufacturing.spinal.fct_spinal_parameter_records
@@ -280,6 +323,9 @@ def job():
     GROUP BY STATION_NAME, PARAMETER_NAME
     """
 
+    ########################################################################################
+    # Query 210 - Every Hour
+    ########################################################################################
     query_210 = f"""
     SELECT COUNT(DISTINCT product_serial) as COUNT, STATION_NAME, PARAMETER_NAME
     FROM manufacturing.spinal.fct_spinal_parameter_records
@@ -311,6 +357,9 @@ def job():
     ORDER BY COUNT DESC
     """
 
+    ########################################################################################
+    # Query 210 - Unique SN COUNT - Every Hour
+    ########################################################################################
     query_210_unique_sn = f"""
     select count(distinct product_serial) as COUNT, station_name
     FROM manufacturing.spinal.fct_spinal_parameter_records
@@ -340,7 +389,10 @@ def job():
     )
     GROUP BY ALL
     """
-    
+
+    ########################################################################################
+    # Query 40 - Fails by Hairpin Origin - Every Hour
+    ########################################################################################
     query_40_hairpin_origin = f"""
     with
 
@@ -430,7 +482,10 @@ def job():
         AND opf.PARAMETER_NAME = 'Force process value'
         group by all
     """
-    
+
+    ########################################################################################
+    # Query 50 - Fails by Hairpin Origin - Every Hour
+    #######################################################################################
     query_50_hairpin_origin = f"""
     with
 
@@ -516,7 +571,10 @@ def job():
         and opf.recorded_at > '{recorded_at}'
         group by opf.station_name, NPR.station_name
     """
-    
+
+    ########################################################################################
+    # Query 65 - Fails by Hairpin Origin - Every Hour
+    ########################################################################################
     query_65_hairpin_origin = f"""
     with
 
@@ -616,7 +674,9 @@ def job():
         group by opsf.station_name, NPR.station_name
     """
 
-    # Execute queries and fetch data into DataFrames
+    ########################################################################################
+    # Execute hourly queries and fetch data into DataFrames
+    ########################################################################################
     df_20 = pd.read_sql(query_20, conn)
     df_40 = pd.read_sql(query_40, conn)
     df_50 = pd.read_sql(query_50, conn)
@@ -626,31 +686,47 @@ def job():
     df_100 = pd.read_sql(query_100, conn)
     df_110 = pd.read_sql(query_110, conn)
     df_210 = pd.read_sql(query_210, conn)
-    
+
     df_210_unique_sn = pd.read_sql(query_210_unique_sn, conn)
     df_40_hairpin_origin = pd.read_sql(query_40_hairpin_origin, conn)
     df_50_hairpin_origin = pd.read_sql(query_50_hairpin_origin, conn)
     df_65_hairpin_origin = pd.read_sql(query_65_hairpin_origin, conn)
 
-
+    ########################################################################################
     # Combine DataFrames
-    df_combined = pd.concat([df_20, df_40, df_50, df_60, df_65, df_80, df_100, df_110, df_210], ignore_index=True)
+    ########################################################################################
+    df_combined = pd.concat(
+        [df_20, df_40, df_50, df_60, df_65, df_80, df_100, df_110, df_210],
+        ignore_index=True,
+    )
 
-    df_combined["PARAMETER_NAME"] = df_combined["ALARM_DESCRIPTION"].fillna(df_combined["PARAMETER_NAME"])
+    df_combined["PARAMETER_NAME"] = df_combined["ALARM_DESCRIPTION"].fillna(
+        df_combined["PARAMETER_NAME"]
+    )
     df_combined.drop(columns=["ALARM_DESCRIPTION"], inplace=True)  # Remove old column
     df_combined = df_combined[df_combined["COUNT"] > 0]
 
-
-
+    ########################################################################################
     # Sort combined DataFrame by 'COUNT' column
-    if 'COUNT' in df_combined.columns:
-        df_combined = df_combined.sort_values(['COUNT'], ascending=False, ignore_index=True)
+    ########################################################################################
+    if "COUNT" in df_combined.columns:
+        df_combined = df_combined.sort_values(
+            ["COUNT"], ascending=False, ignore_index=True
+        )
 
+    ########################################################################################
     # Aggregate total failures per station (without duplicates)
+    ########################################################################################
     df_sum = df_combined.groupby("STATION_NAME")["COUNT"].sum().reset_index()
 
+    ########################################################################################
     # Merge unique product serial failures for Station 210
-    if not df_210_unique_sn.empty and "STATION_NAME" in df_210_unique_sn.columns and "COUNT" in df_210_unique_sn.columns:
+    ########################################################################################
+    if (
+        not df_210_unique_sn.empty
+        and "STATION_NAME" in df_210_unique_sn.columns
+        and "COUNT" in df_210_unique_sn.columns
+    ):
         df_210_unique_sn = df_210_unique_sn.rename(columns={"COUNT": "FAIL_COUNT"})
 
         # Merge Station 210's unique product serial failures into df_sum
@@ -658,122 +734,97 @@ def job():
 
         # Replace total failure count with unique serial count for Station 210
         df_sum["COUNT"] = df_sum["FAIL_COUNT"].fillna(df_sum["COUNT"])
-        
+
         # Drop the temporary column
         df_sum.drop(columns=["FAIL_COUNT"], inplace=True)
     else:
-        print("Warning: STATION_NAME or COUNT column missing from df_210_unique_sn. Falling back to regular sum.")
+        print(
+            "Warning: STATION_NAME or COUNT column missing from df_210_unique_sn. Falling back to regular sum."
+        )
 
+    ########################################################################################
     # Convert NaNs to 0 and ensure integer counts
+    ########################################################################################
     df_sum["COUNT"] = df_sum["COUNT"].fillna(0).astype(int)
 
+    ########################################################################################
     # Sort results
+    ########################################################################################
     df_sum = df_sum[df_sum["COUNT"] > 0]
-    df_sum = df_sum.sort_values(['COUNT'], ascending=False, ignore_index=True)
-    
+    df_sum = df_sum.sort_values(["COUNT"], ascending=False, ignore_index=True)
 
-
-
-    # # Ensure df_unique_serials has necessary columns before proceeding
-    # if 'STATION_NAME' in df_210_unique_sn.columns and 'COUNT' in df_210_unique_sn.columns:
-    #     df_sum = df_210_unique_sn.rename(columns={"COUNT": "FAIL_COUNT"})
-    # else:
-    #     print("Warning: STATION_NAME or COUNT column missing from df_unique_serials. Falling back to regular sum.")
-    #     df_sum = df_combined.groupby('STATION_NAME')['COUNT'].sum().reset_index()
-
+    ########################################################################################
     # Convert DataFrames to a JSON-like format (table-like string)
+    ########################################################################################
     def df_to_table(df):
         table_str = df.to_string(index=False)
-        return table_str 
-    
+        return table_str
 
     df_combined_str = df_to_table(df_combined)
     df_sum_str = df_to_table(df_sum)
-    df_hairpin_origin = pd.concat([df_40_hairpin_origin, df_50_hairpin_origin, df_65_hairpin_origin], ignore_index=True)
+    df_hairpin_origin = pd.concat(
+        [df_40_hairpin_origin, df_50_hairpin_origin, df_65_hairpin_origin],
+        ignore_index=True,
+    )
     df_hairpin_origin_str = df_to_table(df_hairpin_origin)
-    
-    # df_40_hairpin_origin_str = df_to_table(df_40_hairpin_origin)
-    # df_65_hairpin_origin_str = df_to_table(df_65_hairpin_origin)
 
+    ########################################################################################
     # Payload with both DataFrames formatted as tables
+    ########################################################################################
     payload = {
         "blocks": [
-            { "type": "divider" },  # Add a divider to separate sections clearly
+            {"type": "divider"},  # Add a divider to separate sections clearly
             {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
                     # "text": "Fail COUNT by Parameter: " + datetime.now().strftime('%Y-%m-%d %H:00')
-                    "text": "*🚨Fail count by Parameter:* " + recorded_at + " to " + (one_hour_before + timedelta(hours=1)).strftime('%H:00')
-
-                }
+                    "text": "*🚨Fail count by Parameter:* "
+                    + recorded_at
+                    + " to "
+                    + (one_hour_before + timedelta(hours=1)).strftime("%H:00"),
+                },
+            },
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": "```" + df_combined_str + "```"},
+            },
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": "*Fails by Station Pareto:*"},
+            },
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": "```" + df_sum_str + "```"},
+            },
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": "*Fails by Hairpin Station:*"},
             },
             {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "```" + df_combined_str + "```"
-                }
+                    "text": "```" + df_hairpin_origin_str + "```",
+                },
             },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "*Fails by Station Pareto:*"
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "```" + df_sum_str + "```"
-                }
-            
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "*Fails by Hairpin Station:*"
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "```" + df_hairpin_origin_str + "```"
-                }
-            
-            },
-            { "type": "divider" },  # Add a divider to separate sections clearly
+            {"type": "divider"},  # Add a divider to separate sections clearly
         ]
     }
 
+    ########################################################################################
     # Send the payload to Slack using a webhook
-    # url = http_post_hash.url
-    headers = {'Content-type': 'application/json'}
+    ########################################################################################
+    headers = {"Content-type": "application/json"}
     print(f"DEBUG: Sending message to Slack. Token: {slack_token}, Webhook URL: {url}")
     print(f"DATABRICKS_ACCESS_TOKEN Loaded: {DATABRICKS_ACCESS_TOKEN is not None}")
     print(f"SLACK_TOKEN Loaded: {slack_token is not None}")
     print(f"SLACK_WEBHOOK_URL Loaded: {url is not None}")
 
-
     response = requests.post(url, headers=headers, data=json.dumps(payload))
 
-    # print(response.status_code)
-    # print(response.text)
 
-    # t1 = time.time()
-    # print(f'Time taken: {t1-t0} seconds')
-    # print(f"Task completed. Next run scheduled in 1 hour.")
-
-# Schedule the job to run every hour
-# schedule.every(30).seconds.do(job)
-# schedule.every().hour.at(":01").do(job)
-
-# Keep the script running to maintain the schedule
-# while True:
-#     schedule.run_pending()
-#     # time.sleep(15)
-
+########################################################################################
+# RUN job()
+########################################################################################
 job()  # Run the function once
